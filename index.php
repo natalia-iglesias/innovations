38a3c80b8eb54ed884ca20ed9bb87538923642a5
Natalia-iglesias <iglesiasn63@gmial.com>
2024-06-25 11:15:30 -0300

    Primer commit

---
+++ index.php
@@ -0,0 +1,358 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Innovatios Digital Plus</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Sonsie+One" rel="stylesheet" type="text/css" />
+    <link href="./js/bootstrap-5.3.3-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/style.css" />
+    <script src="./js/bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/ecf32bd177.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+
+
+</head>
+
+
+
+
+<body>
+
+    <header id="navbar">
+
+        <nav class="navbar navbar-expand-lg fixed-top navbar-scroll">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="./images/innovations-digital-plus-logo-nav.png" height="60" alt="Logo Innovation digital plus">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#principal">INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#servicios">SERVICIOS</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#tecnologias">TECNOLOGÍAS</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#nosotros">NOSOTROS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#contacto">CONTACTO</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+
+    </header>
+
+
+
+    <section id="principal">
+        <div class="container pt-5 pb-5 h-100">
+            <div class="row pt-5">
+
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="text-center d-flex align-items-center justify-content-center" style="height: 100%;">
+                        <div id="carouselExample" class="carousel slide carousel-fade">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+
+                                    <div class="banner">
+                                        SERVICIOS DIGITALES<br>PARA IMPULSAR<br>EL ÉXITO DE TU EMPRESA
+                                    </div>
+
+                                </div>
+                                <div class="carousel-item">
+
+                                    <div class="banner">
+                                        CAMPAÑAS DE</br>MARKETING DIGITAL</br> A TU MEDIDA
+                                    </div>
+
+                                </div>
+                                <div class="carousel-item">
+
+                                    <div class="banner">
+                                        VENDE MÁS CON<br>TU PROPIA<br>PLATAFORMA ONLINE
+                                    </div>
+
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="col-6 d-flex justify-content-center align-items-center d-none d-md-block">
+                    <img class='img-fluid pt-5 pb-5' src="./images/innovations-digital-plus-logo.png" alt="Logo Innovatios digital plus">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="servicios">
+        <div class="container pt-5 pb-5 h-100">
+            <h3>NUESTROS SERVICIOS</h3>
+            <div class="row mt-5">
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <img class='img-fluid pt-5 pb-5' src="./images/desarrollo-web.png" alt="Desarrollo web">
+                    </div>
+
+                    <div class="servivios-text">
+                        Desarrollo web
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <img class='img-fluid pt-5 pb-5' src="./images/diseno-comunicacion.png" alt="Diseño y comunicación">
+                    </div>
+                    <div class="servivios-text">
+                        Diseño y comunicación
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <img class='img-fluid pt-5 pb-5' src="./images/servicio-marketing.png" alt="Servicios de marketing">
+                    </div>
+                    <div class="servivios-text">
+                        Servicio de marketing
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <img class='img-fluid pt-5 pb-5' src="./images/plataforma-crm.png" alt="Plataforma crm">
+                    </div>
+                    <div class="servivios-text">
+                        Desarrollos de CRM
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    </section>
+    <!-- <section id="tecnologias">
+        <div class="container pt-5 pb-5 h-100">
+            <h3>TECNOLOGÍAS QUE USAMOS</h3>
+            <div class="row mt-5">
+                <div class="col-12 col-sm-12 col-md-6" style="border: solid 1px #fff;">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <h3>ACA VAN LOS ICONOS O LOGOS</br>DE LAS DESARROLLO WEB</h3>
+                    </div>
+
+                    <div class="servivios-text">
+                        Desarrollo web
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6" style="border: solid 1px #fff;">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <h3>ACA VAN LOS ICONOS O LOGOS</br>DE DISEÑO</h3>
+                    </div>
+                    <div class="servivios-text">
+                        Diseño y comunicación
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6" style="border: solid 1px #fff;">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <h3>ACA VAN LOS ICONOS O LOGOS</br>DE MARKETING</h3>
+                    </div>
+                    <div class="servivios-text">
+                        Servicio de marketing
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-6" style="border: solid 1px #fff;">
+                    <div class="col-12 d-flex justify-content-center align-items-center">
+                        <h3>ACA VAN LOS ICONOS O LOGOS</br>DE CRM</h3>
+                    </div>
+                    <div class="servivios-text">
+                        Desarrollos de CRM
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    </section> -->
+    <section id="nosotros">
+        <div class="container col-12 col-sm-12 col-md-6 pt-5 pb-5 h-100">
+        
+            <div class="text-seccion-name">SOBRE NOSOTROS</div>
+            
+            <div class="text-seccion">
+                En Innovations Digital Plus, somos más que una empresa de desarrollo web y marketing digital;
+                somos tu socio estratégico en el mundo digital. Con un equipo de expertos apasionados y creativos, 
+                nos dedicamos a transformar tus ideas en realidades digitales y a llevar tu marca a nuevos niveles de éxito.
+            </div>
+            <div class="text-seccion">
+                Nuestra misión es ofrecer soluciones digitales innovadoras y efectivas que impulsen el crecimiento de nuestros clientes. 
+                Nos comprometemos a proporcionar servicios de alta calidad que no solo cumplan, sino que superen las expectativas, 
+                ayudando a nuestros clientes a destacar en un mercado cada vez más competitivo.
+            </div>
+            <div class="text-seccion">
+                En Innovations Digital Plus, cada proyecto es una oportunidad para innovar y ofrecer soluciones únicas. 
+                Nos enorgullece nuestra capacidad de escuchar y entender las necesidades de nuestros clientes, 
+                para luego diseñar estrategias personalizadas que generen resultados tangibles.
+            </div>
+        </div>
+    </section>
+
+    
+    <section id="contacto" style="height: 500px;">
+        <div class="container pt-5 pb-5 h-100">
+            <h3>CONTACTANOS</h3>
+            <form id="formulario" method="post">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label" style="color: black;">Nombre:</label>
+                            <input type="text" class="form-control" id="nombre" placeholder="Tu nombre">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="email" class="form-label" style="color: black;">Dirección E-mail:</label>
+                            <input type="email" class="form-control" id="email" placeholder="nombre@ejemplo.com">
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="mb-3">
+                            <label for="telefono" class="form-label" style="color: black;">Télefono:</label>
+                            <input type="text" class="form-control" id="telefono" placeholder="Télefono">
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="mb-3">
+                            <label for="mensaje" class="form-label" style="color: black;">Mensaje:</label>
+                            <textarea class="form-control" id="mensaje" rows="4"></textarea>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row justify-content-end">
+                    <div class="col-4 text-end">
+                        <button type="button" id="enviarFormulario" class="btn btn-secondary">Enviar mensaje</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <footer class="footer fondo-footer">
+
+        <div class="container pt-3 pb-3">
+
+            <div class="row">
+                <div class="col order-first">
+                    <a class="navbar-brand" href="#">
+                        <img src="./images/innovations-digital-plus-logo-nav.png" height="60" alt="Logo innovatios digital plus">
+                    </a>
+                </div>
+                <div class="col">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link-footer" aria-current="page" href="#principal"><i class="fa-solid fa-chevron-right"></i>&nbsp;INICIO</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-footer" aria-current="page" href="#servicios"><i class="fa-solid fa-chevron-right"></i>&nbsp;SERVICIOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-footer" aria-current="page" href="#tecnologias"><i class="fa-solid fa-chevron-right"></i>&nbsp;TECNOLOGÍAS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-footer" aria-current="page" href="#nosotros"><i class="fa-solid fa-chevron-right"></i>&nbsp;NOSOTROS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link-footer" aria-current="page" href="#contacto"><i class="fa-solid fa-chevron-right"></i>&nbsp;CONTACTO</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col" style="color: #FFF; line-height: 32px;">
+                    <i class="fa-solid fa-phone"></i>&nbsp;+54 341 7801621</br>
+                    <i class="fa-solid fa-envelope"></i>&nbsp;<a href="mailto:info@innovationdigitalplus.com" style="color: #FFF; text-decoration: none;">info@innovationdigitalplus.com</a></br>
+                    <i class="fa-solid fa-location-dot"></i>&nbsp;Rosario, Santa fe
+                </div>
+                <div class="col order-last">
+                    <div class="social-icons d-flex justify-content-end">
+                        <i class="fa-brands fa-square-whatsapp fa-2x p-2"></i>
+                        <i class="fa-brands fa-facebook fa-2x p-2"></i>
+                        <i class="fa-brands fa-square-instagram fa-2x p-2"></i>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <a href="https://wa.me/543417801621?text=Solicito%20informacion" class="whatsapp" target="_blank">
+            <i class="fa fa-whatsapp whatsapp-icon"></i>
+        </a>
+
+    </footer>
+</body>
+<script>
+    const myCarouselElement = document.querySelector('#carouselExample')
+
+    const carousel = new bootstrap.Carousel(myCarouselElement, {
+        interval: 5000,
+        ride: 'carousel',
+        touch: false
+
+    })
+</script>
+<script>
+    window.addEventListener('scroll', function() {
+        var navbar = document.querySelector('.navbar');
+        if (window.scrollY > 90) { // Cambia 50 por el número de píxeles que quieras para que el cambio ocurra
+            navbar.classList.remove('navbar-scroll');
+            navbar.classList.add('navbar-trasparente');
+        } else {
+            navbar.classList.add('navbar-scroll');
+            navbar.classList.remove('navbar-trasparente');
+        }
+    });
+</script>
+
+<script>
+$(document).ready(function() {
+    $('#enviarFormulario').click(function() {
+        // Obtener los valores del formulario
+        var nombre = $('#nombre').val();
+        var email = $('#email').val();
+        var telefono = $('#telefono').val();
+        var mensaje = $('#mensaje').val();
+       // Enviar los datos por AJAX
+        $.ajax({
+            url: 'send_form.php',
+            method: 'POST',
+            data: {
+                nombre: nombre,
+                email: email,
+                telefono: telefono,
+                mensaje: mensaje
+            },
+            success: function(response) {
+                // Manejar la respuesta del servidor aquí
+                console.log(response);
+            },
+            error: function(xhr, status, error) {
+                // Manejar errores aquí
+                console.error(xhr.responseText);
+            }
+        });
+    });
+});
+</script>
+
+
+</html>
